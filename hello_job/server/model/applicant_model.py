0ebac05b2ec47a8a500f4e8cadd2ab48e7099838
lichaohero <1037827974@qq.com>
2020-01-25 11:01:25 +0800

    数据库备份

--- hello_job/server/model/applicant_model.py
+++ hello_job/server/model/applicant_model.py
@@ -16,34 +16,34 @@ class UserModel:
         self.cur.close()
         self.db.close()
 
-    def regist(self, name, passwd):
-        sql = "select * from applicant where name='%s';" % name
-        self.cur.execute(sql)
-        if self.cur.fetchone():
-            return False
-        sql = 'insert into applicant (name,passwd) values (%s,%s);'
-        try:
-            self.cur.execute(sql, [name, passwd])
-            self.db.commit()
-            return True
-        except:
-            self.db.rollback()
-            return False
-
-    def login(self, name, passwd):
-        sql = "select * from applicant where name='%s' and password='%s';" % (name, passwd)
-        self.cur.execute(sql)
-        if self.cur.fetchone():
-            sql = "update applicant set login_time=now() where name='%s';" % name
-            return True
-        sql = "select * from enterprise where name='%s' and hr_password='%s';" % (name, passwd)
-        self.cur.execute(sql)
-        if self.cur.fetchone():
-            sql = "update * from "
-            return True
-
-        else:
-            return False
+    # def regist(self, name, passwd):
+    #     sql = "select * from applicant where name='%s';" % name
+    #     self.cur.execute(sql)
+    #     if self.cur.fetchone():
+    #         return False
+    #     sql = 'insert into applicant (name,passwd) values (%s,%s);'
+    #     try:
+    #         self.cur.execute(sql, [name, passwd])
+    #         self.db.commit()
+    #         return True
+    #     except:
+    #         self.db.rollback()
+    #         return False
+    #
+    # def login(self, name, passwd):
+    #     sql = "select * from applicant where name='%s' and password='%s';" % (name, passwd)
+    #     self.cur.execute(sql)
+    #     if self.cur.fetchone():
+    #         sql = "update applicant set login_time=now() where name='%s';" % name
+    #         return True
+    #     sql = "select * from enterprise where name='%s' and hr_password='%s';" % (name, passwd)
+    #     self.cur.execute(sql)
+    #     if self.cur.fetchone():
+    #         sql = "update * from "
+    #         return True
+    #
+    #     else:
+    #         return False
 
     def user_information_judgment(self, name, passwd):
         """
