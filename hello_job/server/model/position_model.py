afb573a9d41252df191a32e14a243609c01f6951
lichaohero <1037827974@qq.com>
2020-02-04 10:55:59 +0800

    修改db

--- hello_job/server/model/position_model.py
+++ hello_job/server/model/position_model.py
@@ -1,27 +1,29 @@
 import pymysql
 from hello_job.config import host, port, user, password, database
 
+db = pymysql.connect(host=host,
+                     port=port,
+                     user=user,
+                     password=password,
+                     database=database,
+                     charset='utf8')
+
+cur = db.cursor()
+
 
 class PositionModel:
     def __init__(self):
-        self.db = pymysql.connect(host=host,
-                                  port=port,
-                                  user=user,
-                                  password=password,
-                                  database=database,
-                                  charset='utf8')
-
-        self.cur = self.db.cursor()
+        pass
 
     def close(self):
-        self.cur.close()
-        self.db.close()
+        cur.close()
+        db.close()
 
     def get_position(self, account, position, salary, enterprise):
         if not position and not salary and not enterprise:
             sql = "select wanted_position from applicant where account='%s';" % account
-            self.cur.execute(sql)
-            result = self.cur.fetchone()
+            cur.execute(sql)
+            result = cur.fetchone()
             position = result[0]
 
         sql = "select position.name,enterprise.enterprise_name,position.month_pay,position.content,hr.name from position " \
@@ -35,26 +37,26 @@ class PositionModel:
         if enterprise:
             sql += " and enterprise.enterprise_name regexp '%s'" % (r'.*' + enterprise + '.*')
         print(sql)
-        self.cur.execute(sql)
-        return self.cur.fetchall()
+        cur.execute(sql)
+        return cur.fetchall()
 
     def add_position(self, name, month_pay, content, hr_id, enterprise_id):
         sql = "insert into position (name,month_pay,content,hr_id,enterprise_id) values ('%s','%s','%s',%s,%s)" % (
             name, month_pay, content, hr_id, enterprise_id)
         try:
             print(sql)
-            self.cur.execute(sql)
-            self.db.commit()
+            cur.execute(sql)
+            db.commit()
             return 1
         except:
-            self.db.rollback()
+            db.rollback()
             return 0
 
     def get_hr(self, account):
         sql = "select * from hr where hr_account = '%s'" % account
         print(sql)
-        self.cur.execute(sql)
-        return self.cur.fetchone()
+        cur.execute(sql)
+        return cur.fetchone()
 
 
 if __name__ == '__main__':
