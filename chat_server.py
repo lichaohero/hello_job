71e78b95de9cef1e6b1417e4c40bd76da0c2f3f5
sunguojian <1422413277@qq.com>
2020-02-19 02:43:24 +0800

    聊天服务器

--- chat_server.py
+++ chat_server.py
@@ -1,21 +1,25 @@
 from socket import *
 import os
 import db
+import json
+import time
 
-ADDR = ('0.0.0.0', 8888)
+ADDR = ('0.0.0.0', 8402)
 
 # 创建用户存储字典 {name:address}
 allapps = {}
 allhrs = {}
 db = db.Database()
 
-def do_login(s,account,password,addr):
+# 登录账户与ip映射
+def do_login(account,password,addr):
     if db.selecthrcount(account,password)[0][0] == 1:
         allhrs[account] = addr
-        s.sendto(b'OK',allhrs[account])
+        return True
     elif db.selectappcount(account,password)[0][0] == 1:
         allapps[account] = addr
-        s.sendto(b'OK',allapps[account])
+        return True
+    return False
 
 #账户登录后立即加载聊天记录
 def initchatrecord(s,fromaccount,toaccount,addr):
@@ -23,46 +27,46 @@ def initchatrecord(s,fromaccount,toaccount,addr):
     if fromaccount in allapps:
         allapps[fromaccount] = addr
         for i in range(len(records)):
-            s.sendto(records[i][0].encode(), allapps[fromaccount])
+            s.sendto((str(records[i][4])+"\n"+records[i][0]).encode(), allapps[fromaccount])
     else:
         allhrs[fromaccount] = addr
         for i in range(len(records)):
-            s.sendto(records[i][0].encode(), allhrs[fromaccount])
+            s.sendto((str(records[i][4])+"\n"+records[i][0]).encode(), allhrs[fromaccount])
 
-# 将所有hr和app用户添加到用户存储字典中
+# 将所有求职者添加到用户存储字典中
 def addapps():
     allapp = db.selallapps()
     for app in allapp:
         allapps[app[0]] = ''
     return allapps
-
+# 将所有hr添加到用户存储字典中
 def addhrs():
     allhr = db.selectallhrs()
     for hr in allhr:
         allhrs[hr[0]] = ''
     return allhrs
 
-# 求职者和hr相互发消息
-def do_chat(s, fromaccount, text, toaccount, addr=''):
-    msg = "\n%s : %s" % (fromaccount, text)
+# 求职者和hr相互发消息并保存消息记录
+def do_chat(s, fromaccount, text, toaccount, send_time,addr=''):
+    msg = "\n%s : %s : %s" % (str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())), fromaccount,text)
     if fromaccount in allhrs:
         for i in allapps:
             if i != fromaccount and i == toaccount:
                 if addr == '' or addr is None:
-                    db.insertchatrecord(fromaccount, toaccount, text, 0)
+                    db.insertchatrecord(fromaccount, toaccount, text, 0,send_time)
                     return
                 allapps[toaccount] = addr
-                db.insertchatrecord(fromaccount, toaccount, text, 1)
+                db.insertchatrecord(fromaccount, toaccount, text, 1,send_time)
                 db.updatechatrecord(toaccount)
                 s.sendto(msg.encode(), allapps[toaccount])
     if fromaccount in allapps:
         for i in allhrs:
             if i != fromaccount and i == toaccount:
                 if allhrs[toaccount] == '' or allhrs[toaccount] is None:
-                    db.insertchatrecord(fromaccount,toaccount,text,0)
+                    db.insertchatrecord(fromaccount,toaccount,text,0,send_time)
                     return
                 else:
-                    db.insertchatrecord(fromaccount, toaccount, text, 1)
+                    db.insertchatrecord(fromaccount, toaccount, text, 1,send_time)
                     db.updatechatrecord(toaccount)
                     s.sendto(msg.encode(),allhrs[toaccount])
 
@@ -71,26 +75,28 @@ def do_request(s):
     while True:
         # 所有请求都在这里接受
         data, addr = s.recvfrom(1024)
-        tmp = data.decode().split(' ')  # 拆分请求
-        # 任务分发 (CHAT QUIT)
-        if tmp[0] == "LOGIN":
-            do_login(s,tmp[1],tmp[2],addr)
-        elif tmp[0] == "CHAT":
-            if tmp[1] in allapps:
-                do_chat(s, tmp[1], tmp[2],tmp[3],allhrs[tmp[3]])
-            elif tmp[1] in allhrs:
-                do_chat(s, tmp[1], tmp[2], tmp[3], allapps[tmp[3]])
-        elif tmp[0] == "INITCHATRECORD":
-            if tmp[1] in allapps:
-                initchatrecord(s,tmp[1],tmp[2],addr)
-            elif tmp[1] in allhrs:
-                initchatrecord(s, tmp[1], tmp[2], addr)
-
+        request = json.loads(data)
+        if request["request_type"] == "LOGIN":
+            if do_login(request["data"]["username"],request["data"]["pwd"],addr):
+                s.sendto(b'LOGIN OK',addr)
+        elif request["request_type"] == "INITCHATRECORD":
+            initchatrecord(s,request["From"],request["To"],addr)
+        elif request["request_type"] == "p_send_msg":
+            if request["data"]["From"] in allapps:
+                do_chat(s, request["data"]["From"], request["data"]["send_content"],request["data"]["To"],request["data"]["send_time"],allhrs[request["data"]["To"]])
+            elif request["data"]["From"] in allhrs:
+                do_chat(s, request["data"]["From"], request["data"]["send_content"], request["data"]["To"], request["data"]["send_time"],allapps[request["data"]["To"]])
+        elif request["request_type"] == "QUIT":
+            if request["account"] in allhrs:
+                allhrs[request["account"]] = ""
+            elif request["account"] in allapps:
+                allapps[request["account"]] = ""
 
 # 搭建网络
 def main():
     # udp服务端网络
     s = socket(AF_INET, SOCK_DGRAM)
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     s.bind(ADDR)
 
     addapps()
